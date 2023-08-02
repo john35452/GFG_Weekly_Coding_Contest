3acbc43152a1deb3f47c9594288bdc8595a30d51
john35452 <john354525@gmail.com>
2023-08-03 00:58:02 +0800

    2023/08/02 Add weekly contest 65

---
+++ gfg-weekly-coding-contest-65/Prime List.py
@@ -0,0 +1,55 @@
+from typing import Optional
+
+"""
+
+Definition for singly Link List Node
+class Node:
+    def __init__(self,x):
+        self.data=x
+        self.next=None
+
+You can also use the following for printing the link list.
+printList(node)
+"""
+
+maxv = 1000
+hasCompute = False
+prime = []
+class Solution:
+    # Version 1: Sieve
+    # Let v be the range of the value
+    # TC: O(vloglogv + nlogv), SC: O(v)
+    def primeList(self, head : Optional['Node']) -> Optional['Node']:
+        # code here
+        from bisect import bisect_left
+        global maxv
+        global hasCompute
+        global prime
+        
+        if not hasCompute:
+            hasCompute = True
+            spf = list(range(maxv + 1))
+            for i in range(2, maxv + 1):
+                if spf[i] == i:
+                    for j in range(i * i, maxv + 1, i):
+                        spf[j] = min(spf[j], i)
+            
+            for i in range(2, maxv + 1):
+                if spf[i] == i:
+                    prime.append(i)
+        
+        current = head
+        while current:
+            index = bisect_left(prime, current.data)
+            if index > 0 and index < len(prime):
+                if current.data - prime[index - 1] <= prime[index] - current.data:
+                    current.data = prime[index - 1]
+                else:
+                    current.data = prime[index]
+            elif index > 0:
+                current.data = prime[index - 1]
+            else:
+                current.data = prime[index]
+            current = current.next
+        return head
+            
